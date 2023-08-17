d27ebe8b99a6b73922e8fd919b347599321bc227
shay-halimi <116021094+shayhalimibusiness@users.noreply.github.com>
2023-08-17 16:08:36 +0300

    Builder

--- CatanGame/Builder/Builder.cs
+++ CatanGame/Builder/Builder.cs
@@ -1,10 +1,27 @@
+using CatanGame.Enums;
+using CatanGame.Evaluator;
+using CatanGame.Game;
+using CatanGame.History;
+using CatanGame.Judge;
+using CatanGame.Player;
 using CatanGame.System;
+using CatanGame.System.Board;
+using CatanGame.System.Cards;
+using CatanGame.UI;
 
 namespace CatanGame.Builder;
 
 public class Builder
 {
+    public Dictionary<EPlayer, ICards>? AllCards { get; set; }
+    public IBoard? Board { get; set; }
     public ISystem? System { get; set; }
+    public IUi? Ui { get; set; }
+    public IEvaluator? Evaluator { get; set; }
+    public IJudge? Judge { get; set; }
+    public IEnumerable<IPlayer>? Players { get; set; }
+    public IGame? Game { get; set; }
+    public IHistory? History { get; set; }
     public string? FinalScorePath { get; set; }
     public string? TurnTimesPath { get; set; }
 }
--- CatanGame/Builder/BuilderFactory.cs
+++ CatanGame/Builder/BuilderFactory.cs
@@ -1,18 +1,56 @@
+using CatanGame.Enums;
+using CatanGame.Game;
+using CatanGame.History;
+using CatanGame.Player;
 using CatanGame.System;
+using CatanGame.System.Board;
+using CatanGame.System.Cards;
+using CatanGame.Utils;
 
 namespace CatanGame.Builder;
 
 public static class BuilderFactory
 {
     private const string HistoryDirPath = @"C:\Users\shay.halimi\Documents\Catan\Catan\History";
-    
-    public static Builder CreateFullBuilder()
+
+    public static Builder Create1PlayerEmptyGameFullBuilder()
     {
-        return new Builder()
+        var builder = new Builder()
         {
             FinalScorePath = Path.Combine(HistoryDirPath, "FinalScores.txt"),
             TurnTimesPath = Path.Combine(HistoryDirPath, "TurnTimes.txt"),
-            System = SystemFactory.Create1PlayerSystem()
+            Board = BoardFactory.CreateRandomBoard(),
+            AllCards = CardsFactory.Create1PlayerBlankAllCards(),
+            Ui = UiFactory.CreateUi(),
         };
+        builder.System = new System.System(builder.Board, builder.AllCards, builder.Ui);
+        builder.Evaluator = new Evaluator.Evaluator(builder.System, EPlayer.Player1);
+        builder.Judge = new Judge.Judge(builder.System, builder.Ui, EPlayer.Player1);
+        
+        return builder;
+    }
+    
+    public static Builder Create1HeuristicPlayerFullBuilder()
+    {
+        var builder = Create1PlayerEmptyGameFullBuilder();
+        builder.Players = new List<IPlayer> { new HeuristicPlayer(builder.Judge!, builder.Evaluator!) };
+        builder.Game = GameFactory.Create1PlayerGame(builder);
+        
+        return builder;
+    }
+    
+    public static Builder Create1ExpectimaxPlayerFullBuilder()
+    {
+        var builder = BuilderFactory.Create1PlayerEmptyGameFullBuilder();
+        builder.Players = new List<IPlayer>
+        {
+            new ExpectimaxPlayer(
+                builder.Judge!, 
+                builder.Evaluator!,
+                builder.System!)
+        };
+        builder.Game = GameFactory.Create1PlayerGame(builder);
+        
+        return builder;
     }
 }
